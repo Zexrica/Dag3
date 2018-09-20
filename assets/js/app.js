ebbc21ffd370917704f7168e09697f11edf69309
Zexrica <krij0046@hz.nl>
2018-09-20 08:44:19 +0200

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+// Opdracht 1
+let number = 7;
+
+if (number%2 == 0) {
+    console.log(`${number} is an even number`);
+} else {
+    console.log(`${number} is an odd number`);
+};
+
+// Opdracht 2
+const programmingIsNotCool = 'Programming is not so cool';
+const programmingIsCool = programmingIsNotCool.replace('not ', '');
+console.log(programmingIsNotCool);
+console.log(programmingIsCool);
+
+// Opdracht 3
+const a = 1400,
+    b = 'Ik woon in Naboo';
+console.log(a == b);
+// Je vergelijkt een string met een number, dit is natuurlijk gewoon onzin
